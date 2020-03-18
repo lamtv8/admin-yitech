e40f06e4be5ebc760d8fdeee1944ea209d9e4ce5
lamtv <lamtvse62635@gmail.com>
2020-03-18 20:27:38 +0700

    update

--- src/static/constant.js
+++ src/static/constant.js
@@ -9,7 +9,7 @@ if (process.env.REACT_APP_ENV === "production") {
 const API = {
   LOGIN: `${HOST}${PORT}/api/auth/login/admin`,
   CHECK_TOKEN: `${HOST}${PORT}/api/v1/users/current_user/`,
-  GET_IMAGE: `http://34.87.12.194/api/admin/users`,
+  GET_IMAGE: `http://35.240.228.194:8760/service-user/api/admin/users`,
   GET_VIDEO_LIST:`${HOST}${PORT}/api/v1/videos/`,
   GET_STREAM: `${HOST}${PORT}/api/v1/stream`
 };
